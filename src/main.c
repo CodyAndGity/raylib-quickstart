3ba932241b387d9c737b0701e945119dbc1216fb
CodyAndGity <mdavis@student.neumont.edu>
2025-04-24 14:45:33 -0600

    Added raylib renderer and custom niko function

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-/*
+﻿/*
 Raylib example file.
 This is an example main file for a simple raylib project.
 Use this as a starting point or replace it with your code.
@@ -8,22 +8,25 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 */
 
 #include "raylib.h"
-
+#include "raymath.h"	// for sin function
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
 int main ()
 {
+	const int screenWidth = 1280;
+	const int screenHeight = 800;
 	// Tell the window to use vsync and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
-
+	
 	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
+	InitWindow(screenWidth, screenHeight, "Hello Raylib");
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
 	// Load a texture from the resources directory
 	Texture wabbit = LoadTexture("wabbit_alpha.png");
+	Texture Niko = LoadTexture("Niko.jpg");
 	
 	// game loop
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
@@ -32,14 +35,22 @@ int main ()
 		BeginDrawing();
 
 		// Setup the back buffer for drawing (clear color and depth buffers)
-		ClearBackground(BLACK);
+		//ClearBackground(BLACK);
 
 		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
+		DrawText("Hello Raylib", 200, 200, 20, WHITE);
 
 		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
-		
+		const int sizeFactor = 5;
+		for (short i = 0;i < 100;i++) {
+		//DrawTexture(wabbit, GetMouseX() + GetRandomValue(0, screenWidth / sizeFactor) - (screenWidth / (sizeFactor*2)), GetMouseY() + GetRandomValue(0, screenHeight / sizeFactor) - (screenHeight / (sizeFactor*2)), WHITE);
+		};
+		for (short i = 0;i < screenWidth;i+=50) {
+			int y = screenHeight  / 2+(sinf(((GetTime()*3) )+i/(100.0f))*200);
+			//DrawTexture(Niko, i, y, WHITE);
+			float scale = 0.5f + sinf(GetTime()) / 2;
+			DrawTextureEx(Niko, (Vector2) { i, y }, (GetTime()*1115), scale, MAGENTA);
+		};
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
 	}
